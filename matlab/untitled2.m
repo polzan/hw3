45c3f286df37c849b6932d1cab39745e6215c671
Riccardo Zanol <ricc@zanol.eu>
2016-05-07 15:43:06 +0200

    Rename some variables, use impz
    Conflicts:
    	matlab/untitled2.m

--- matlab/untitled2.m
+++ matlab/untitled2.m
@@ -3,15 +3,14 @@ close all; clear all; clc;
 %freq response of Q_c(f)
 T = 1;
 up_factor = 4;
-T_4 = T/up_factor;      % F_4 = 4
-b = zeros(1,11);
-b(1,11) = 0.7424;   %beta
-a = [1, -0.67];     %1-alfa
-Q_f = freqz(b,a,'whole',2000);
-q = ifft(Q_f);
-q = q(1:27);            %works at T_4
-f = linspace(0,2/T,1000);
+T_Q = T/up_factor;      % F_4 = 4
 
+Q_c_num = [zeros(1,10), 0.7424];   %beta
+Q_c_den = [1, -0.67];     %1-alfa
+
+q_c = impz(Q_c_num, Q_c_den, 27);
+
+[Q_f_plot, f_plot] = freqz(Q_c_num, Q_c_den, 1000, 1/(2*T_Q));
 
 %modulator
 D = 13;
@@ -24,21 +23,21 @@ a_k = upsample(a_k,up_factor);
 
 %compute additional params.
 SNR_target = 11;
-E_q = q.'*conj(q);      %energy of q
+E_q = q_c.'*conj(q_c);      %energy of q
 sigma2_a = 1/sqrt(2);%/up_factor;    %%%%%%%%%%%????????
 %%%%lesson 21/04??
 sigma2_omega = 10*log10(E_q*sigma2_a) - SNR_target;       %sigma2 = Msc*Q0 - SNR in dB
 sigma2_omega = 10^(sigma2_omega/10);            %%%linear noise power
-N0 = sigma2_omega*T_4;
-omega = sqrt(sigma2_omega).*(randn(length(a_k)+length(q)-1,1) + 1j*(randn(length(a_k)+length(q)-1,1)));
+N0 = sigma2_omega*T_Q;
+omega = sqrt(sigma2_omega).*(randn(length(a_k)+length(q_c)-1,1) + 1j*(randn(length(a_k)+length(q_c)-1,1)));
 
 %rc signal
-r_c = conv(a_k,q) + omega;
+r_c = conv(a_k,q_c) + omega;
 
 
 %match filter
 M1 = 19;        %at least 12
-q_match = fliplr(q(1:M1)');  %already shifted
+q_match = fliplr(q_c(1:M1)');  %already shifted
 r_r = conv(r_c,q_match);
 to = 19;  %%%%%%dal grafico di r_qc??? = E_q??(8.24) real t0= M1??
 r_sampled = zeros(1,length(a_k)/4);
@@ -47,7 +46,7 @@ for i = 1:length(a_k)/4      %%sampling in to+i*T
 end
 
 %%%c estimation%%%
-r_qc = conv(q,q_match);      %%real t=14??
+r_qc = conv(q_c,q_match);      %%real t=14??
 
 r_qc = decimate(circshift(r_qc,length(r_qc)-to+1),4);
 r_qc = circshift(r_qc,length(r_qc)/2);
@@ -65,23 +64,23 @@ p_bit_no_c = length(find(bit_est2 -PN ~= 0))/length(PN)
 
 figure;
 subplot(1,3,1);
-plot(f, 10*log10(abs(Q_f(1:1000))));    %2/T but qc works T/4 >> plot half a period
-xlabel('n/T'); ylabel('|Q_c(f)|');
+plot(f_plot, 20*log10(abs(Q_f_plot)));    %2/T but qc works T/4 >> plot half a period
+xlabel('n/T_Q'); ylabel('|Q_c(f)|');
 subplot(1,3,2);
-stem(0:length(q)-1, q);
+stem(0:length(q_c)-1, q_c);
 xlabel('nT'); ylabel('q_c(nT)');
 subplot(1,3,3)
 stem(0:length(q_match)-1, q_match);
 
 %compares error with and without c (something wrong)
-figure(2);
+figure;
 subplot(1,2,1)
 stem(bit_est-PN);
 subplot(1,2,2)
 stem(bit_est2-PN);
 
 %interf before anf after c
-figure(3);
+figure;
 subplot(1,2,1)
 stem(0:length(r_qc)-1,r_qc);
 subplot(1,2,2)
