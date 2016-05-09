fc9d1a015e7dcb2dee725b9f116fc2e758811cb5
Riccardo Zanol <ricc@zanol.eu>
2016-05-09 15:47:39 +0200

    Also return the detected symbols

--- matlab/QPSKdemodulator.m
+++ matlab/QPSKdemodulator.m
@@ -1,19 +1,25 @@
-function [ a ] = QPSKdemodulator(data)
-a = zeros(2*length(data),1);
+function [bits, detected_syms] = QPSKdemodulator(data)
+bits = zeros(2*length(data),1);
+detected_syms = zeros(length(data), 1);
+sym_d = 1;
 v = 0;
 for i = 1:length(data)
     if real(data(i)) > 0 && imag(data(i)) > 0
-        a(v+i) = 0;
-        a(v+i+1) = 0;
+        bits(v+i) = 0;
+        bits(v+i+1) = 0;
+        detected_syms(i) = sym_d * (1 + 1j);
     elseif  real(data(i)) > 0 && imag(data(i)) < 0
-        a(v+i) = 1;
-        a(v+i+1) = 0;
+        bits(v+i) = 1;
+        bits(v+i+1) = 0;
+        detected_syms(i) = sym_d * (1 - 1j);
     elseif real(data(i)) < 0 && imag(data(i)) > 0
-        a(v+i) = 0;
-        a(v+i+1) = 1;
+        bits(v+i) = 0;
+        bits(v+i+1) = 1;
+        detected_syms(i) = sym_d * (-1 + 1j);
     else
-        a(v+i) = 1;
-        a(v+i+1) = 1;
+        bits(v+i) = 1;
+        bits(v+i+1) = 1;
+        detected_syms(i) = sym_d * (-1 - 1j);
     end
     v = v+1;
 end
