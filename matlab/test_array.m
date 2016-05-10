f29185f518a31fbca2db32f8d94f6fde2faa5340
Riccardo Zanol <ricc@zanol.eu>
2016-05-10 15:02:42 +0200

    Edit the test/examle for ArrayWithIndices

--- matlab/test_array.m
+++ matlab/test_array.m
@@ -2,17 +2,25 @@ close all; clear all; clc;
 
 [b,a] = transmitter_tf();
 
-h = ArrayWithIndices(impz(b,a));
+h = ArrayWithIndices(impz(b,a), -10);
 
-h.offset = -5;
+h.alignOffset(-1);
 
 g = h.flip();
 
 figure;
 hold on;
-stem(h.indices, h.all);
-h.autoPadding = true;
-stem(-50:50, h(-50:50));
+stem(h.getIndices(), h.getAll());
+figure;
+h.setAutoPadding(true);
+stem(-50:50, h.getData(-50:50));
 
 figure;
-stem(g.indices, g.all);
+stem(g.getIndices(), g.getAll());
+
+h.setData(-50, 1)
+h.setData(-40:-36, -1*ones( 5, 1));
+figure;
+hold on;
+stem(h.getIndices(), h.getAll());
+
