dbb0e9cfd0e4159654db45dacaa1189fb7176f0e
Riccardo Zanol <ricc@zanol.eu>
2016-05-11 10:29:32 +0200

    Work also with an empty b, return the y_k

--- matlab/dfe_filtering.m
+++ matlab/dfe_filtering.m
@@ -1,22 +1,22 @@
-function detected_syms = dfe_filtering(c, b, x, D)
-C = dfilt.df2(c, 1);
-%C.PersistentMemory = true;
-B = dfilt.df2(b, 1);
-B.PersistentMemory = true;
-
-Nsym = length(x);
-detected_syms = zeros(Nsym, 1);
-xff = C.filter(x);
-%xfb = zeros(Nsym, 1);
-%y = zeros(Nsym, 1);
-for i=0:Nsym-1
-     if i-D >= 0
-         
-        xfb_i = B.filter(detected_syms(i-D+1));
-    else
-        xfb_i = B.filter(0);
+function [detected_syms, y] = dfe_filtering(c, b, x, D)
+xff = filter(c, 1, x);
+if isempty(b) || all(b == 0)
+    y = xff;
+    [~, detected_syms] = QPSKdemodulator(y);
+else
+    Nsym = length(x);
+    detected_syms = zeros(Nsym, 1);
+    y = zeros(Nsym, 1);
+    B = dfilt.df2(b, 1);
+    B.PersistentMemory = true;
+    for k=0:Nsym-1
+        if k-D >= 0
+            xfb_k = B.filter(detected_syms(k-D+1));
+        else
+            xfb_k = B.filter(0);
+        end
+        y(k+1) = xff(k+1) + xfb_k;
+        [~, detected_syms(k+1)] = QPSKdemodulator(y(k+1));
     end
-    y_i = xff(i+1) + xfb_i;
-    [~, detected_syms(i+1)] = QPSKdemodulator(y_i);
 end
 end
