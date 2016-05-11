28f273d8ad3c213c22a4af808ec8879c0d1f0179
Riccardo Zanol <ricc@zanol.eu>
2016-05-11 12:18:48 +0200

    Update the DFE filters construction

--- matlab/build_dfe_filters.m
+++ matlab/build_dfe_filters.m
@@ -1,39 +1,55 @@
-function [c,b] = build_dfe_filters(sigma2_a, N0, q_match_impz, h, D, M1, M2)
-%assert(M1 <= D);
-N1 = abs(min(h.getIndices()));
-N2 = abs(max(h.getIndices()));
+function [c,b] = build_dfe_filters(qc, gm, t0, sigma2_a, N0, D, M1, M2)
+h_T_Q = conv(qc, gm);
+h = downsample(h_T_Q, 4, mod(t0, 4));
+t0_sampled = floor(t0/4);
+N1 = t0_sampled;
+N2 = length(h)-1 - t0_sampled;
+assert(length(h) == N1 + N2 + 1);
+if nargin < 7
+    M2 = -D + N2 + M1 -1;
+end
+r_gm = xcorr(gm, 'none');
+r_gm_zero_offset = (length(r_gm)-1)/2;
+r_gm_sampled = downsample(r_gm, 4, mod(r_gm_zero_offset, 4));
+r_gm_sampled_zero_offset = floor(r_gm_zero_offset / 4);
 
-h_ = h.flip();
-h_.setAutoPadding(true);
-P = sigma2_a .* conj(h_.getData((-M1+1:0)+D));
+r_ax = sigma2_a .* conj(flip(h));
+r_h = xcorr(h, 'none');
+r_h_zero_offset = (length(r_h)-1)/2;
+r_w = N0 .* r_gm_sampled;
+r_w_zero_offset = r_gm_sampled_zero_offset;
 
-r_w = ArrayWithIndices(N0 .* xcorr(q_match_impz), -length(q_match_impz)+1);
+assert(M1-D <= N1);
+assert(D >= 0);
+assert(D <= N2-1);
+P = r_ax((1:M1) - D + t0_sampled);
+
+assert(D+M2 <= N2-1);
 R = zeros(M1, M1);
-h.setAutoPadding(true);
 for p=0:M1-1;
     for q=0:M1-1
-        s1 = (h.getData((-N1:N2) - p + q))' * h.getData(-N1:N2);
-        if M2 ~= 0
-            s2 = (h.getData((1:M2) +D -p))' * h.getData((1:M2) + D -q);
-        else
+        s1 = r_h(r_h_zero_offset + p-q +1);
+        if M2 == 0
             s2 = 0;
+        else
+            h1 = h((D+1:D+M2) -q + t0_sampled +1);
+            h2 = conj(h((D+1:D+M2) -p + t0_sampled +1));
+            s2 = transpose(h1) * h2;
         end
         
-        R(p+1,q+1) = sigma2_a * (s1 - s2) + r_w.getData(p-q);
+        R(p+1,q+1) = sigma2_a * (s1 - s2) + r_w(p-q+1+r_w_zero_offset);
     end
 end
-h.setAutoPadding(false);
 
 c = linsolve(R, P);
 
-
 if M2 == 0
-    b = [];
-    return;
-end
-h_D = h.translate(D);
-b = zeros(M2, 1);
-for i=1:M2
-    b(i) = - c.' * h_D.getData(i:i+M1-1);
+    b = [0];
+else
+    h_D = h.translate(D);
+    b = zeros(M2, 1);
+    for i=1:M2
+        b(i) = - c.' * h_D.getData(i:i+M1-1);
+    end
 end
 end
