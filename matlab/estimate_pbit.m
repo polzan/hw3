1c7534b6e9ef7bc2d0b76b594a5cc25b6767dd6b
Riccardo Zanol <ricc@zanol.eu>
2016-05-12 15:30:52 +0200

    Add a function to run the simulations until a certain number of errors

---
+++ matlab/estimate_pbit.m
@@ -0,0 +1,12 @@
+function [pbit, tot_errs, tot_bits] = estimate_pbit(simulate_txrx, SNR)
+err_needed = 30;
+blocklength = 1e4;
+tot_errs = 0;
+tot_bits = 0;
+while tot_errs < err_needed
+    [~, err_count] = simulate_txrx(blocklength, SNR);
+    tot_bits = tot_bits + blocklength;
+    tot_errs = tot_errs + err_count;
+end
+pbit = tot_errs / tot_bits;
+end
