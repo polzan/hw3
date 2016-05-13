37145148f4c6b22f8fb657a63f34fb84f19ba41b
Riccardo Zanol <ricc@zanol.eu>
2016-05-13 20:28:13 +0200

    Also return the impulse response

--- matlab/transmitter_tf.m
+++ matlab/transmitter_tf.m
@@ -1,5 +1,6 @@
-function [b,a, qc_length] = transmitter_tf()
+function [b, a, qc_length, qc] = transmitter_tf()
 b = [zeros(1,10), 0.7424];   %beta
 a = [1, -0.67];     %1-alfa
-qc_length = 34;
+qc_length = 34; % After this < 5e-5
+qc = impz(b, a, qc_length);
 end
