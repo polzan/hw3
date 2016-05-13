c70a2b3682d1db4d8c15d1a323a794621b46a7d2
Enrico <polo.enrico@hotmail.it>
2016-05-13 12:12:46 +0200

    added some files

---
+++ matlab/Pbit_estimation.m
@@ -0,0 +1,35 @@
+i=1;
+for SNR = 8:0.5:14
+    M1 = 2;
+    M2 = 2;
+    test_dfe;
+    P_GMDFE(i) = Pbit;
+    P_theo(i) = Pbit_upper_bound;
+    i=i+1;
+end
+i=1;
+for SNR = 8:0.5:14
+    M1 = 2;
+    M2 = 0;
+    test_dfe;
+    P_GMLE(i) = Pbit;
+    i=i+1;
+end
+i=1;
+for SNR = 8:0.5:14
+    M1 = 2;
+    M2 = 4;
+    test_aa;
+    P_GMAA(i) = Pbit;
+    i=i+1;
+end
+semilogy(linspace(8,14,13),P_GMAA,'c');
+hold on
+semilogy(linspace(8,14,13),P_GMDFE,'r');
+hold on
+semilogy(linspace(8,14,13),P_GMLE,'g');
+hold on
+semilogy(linspace(8,14,13),P_theo,'b');
+
+xlabel('SNR_(db)'); legend('AA + DFE @T','MF + DFE','GM + LE','theoretical');
+ylabel('Pbit') 
