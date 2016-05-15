94599615f2010981d9932857f9a78a3718474e2c
Riccardo Zanol <ricc@zanol.eu>
2016-05-15 15:34:06 +0200

    Edit test

--- matlab/test_viterbi_func.m
+++ matlab/test_viterbi_func.m
@@ -3,27 +3,30 @@ close all;
 alphabet = [1+1j; 1-1j; -1-1j; -1+1j];
 %alphabet = [-1,1];
 
-K = 100;
+K = 300;
 L1 = 0;
-L2 = 3;
+L2 = 2;
 
-psi = [0; 0; 1; 0.1];
-t0 = 1;
-D = 1;
+psi = [0; 0; 1.4; 0; 0];
+t0 = 0;
+D = 2;
 
 initial_path_metrics = zeros(4^(L1+L2), 1);
 
 rng(4);
 a_tx = alphabet(round(rand(K, 1) .* 3 + 1));
+w = sqrt(0.1) .* (randn(length(a_tx), 1) + 1j .* randn(length(a_tx), 1));
 
-rho = conv(psi, a_tx);
+rho = conv(psi, a_tx + w);
 
 detector = ViterbiDetector(alphabet, K, L1, L2, psi, t0 + D);
 
 
 [a, final_path_metrics] = detector.detect_symbols(rho, initial_path_metrics);
 
-a_cut_first = a(t0+D+1:length(a));
+output_translation = L2+L1;
+
+a_cut_first = a(max(output_translation+1,1):min(length(a),length(a)+output_translation));
 
 sym_err_count = sum(a_cut_first ~= a_tx(1:length(a_cut_first)));
 Pe = sym_err_count / K;
@@ -37,6 +40,21 @@ stem(0:length(a)-1, real(a));
 ylim([-1.2 1.2]);
 
 figure;
+subplot(1,2,1);
+stem(0:length(a_tx)-1, 1.1.*real(a_tx));
+hold on;
+stem(0:length(a)-1, real(a));
+ylim([-1.2 1.2]);
+
+subplot(1,2,2);
+stem(0:length(a_tx)-1, 1.1.*imag(a_tx));
+hold on;
+stem(0:length(a)-1, imag(a));
+ylim([-1.2 1.2]);
+
+figure;
 scatter(real(rho), imag(rho));
 hold on;
 scatter(real(a), imag(a));
+
+
