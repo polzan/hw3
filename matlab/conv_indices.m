e489c915fa44cd5f7b33140a886e5446fe92198b
Riccardo Zanol <ricc@zanol.eu>
2016-05-15 19:10:33 +0200

    Remove ArrayWithIndices

--- matlab/conv_indices.m
+++
@@ -1,9 +0,0 @@
-function c = conv_indices(h, g)
-minoff = min(h.getOffset(), g.getOffset());
-h.alignOffset(minoff);
-g.alignOffset(minoff);
-c_data = conv(h.getAll(), g.getAll());
-c_off = 2*minoff;
-c = ArrayWithIndices(c_data, c_off);
-end
-
