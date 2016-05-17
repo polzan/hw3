0b8082573e0f89f119ea3f8e78f7dddbf0c8b4e8
Riccardo Zanol <ricc@zanol.eu>
2016-05-17 19:23:08 +0200

    Add the legend

--- matlab/plot_results.m
+++ matlab/plot_results.m
@@ -12,13 +12,15 @@ semilogy(SNRs, pe_aa, 'k-');
 semilogy(SNRs, pe_vit, 'r--');
 semilogy(SNRs, pe_mlm, 'r');
 
-semilogy(SNRs, pe_theor_bound, 'g-');
 semilogy(SNRs, pe_noisi, 'g--');
+semilogy(SNRs, pe_theor_bound, 'g-');
 
 xlabel('SNR [dB]');
 ylabel('Pe');
 grid on;
 xlim([8, 14]);
 ylim([1e-4, 1e-1]);
+
+legend('MF+LE@T', 'MF+DFE@T', 'AAF+DFE@T/2', 'AAF+DFE@T', 'VA', 'FBA', 'MFb-s', 'MFb-t');
 
 print('pe_plot', '-depsc');
