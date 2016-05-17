68b34c63e8de380fe1c14f7505890b65ec77b462
Riccardo Zanol <ricc@zanol.eu>
2016-05-17 09:07:39 +0200

    Add a script to plot the results

---
+++ matlab/plot_results.m
@@ -0,0 +1,23 @@
+close all; clear all; clc;
+
+load('../pe_plots_data.mat');
+
+% Plots
+figure;
+semilogy(SNRs, pe_le, 'b--');
+hold on;
+semilogy(SNRs, pe_dfe, 'b-');
+semilogy(SNRs, pe_aa2, 'k--');
+semilogy(SNRs, pe_aa, 'k-');
+semilogy(SNRs, pe_vit, 'r--');
+semilogy(SNRs, pe_mlm, 'r');
+
+semilogy(SNRs, pe_bounds(:,1), 'g-');
+semilogy(SNRs, pe_sim, 'g--');
+
+xlabel('SNR [dB]');
+ylabel('Pe');
+grid on;
+xlim([8, 14]);
+ylim([1e-4, 1e-1]);
+
