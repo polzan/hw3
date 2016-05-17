f3003bba0c041db284d36a5c86fc62c00b7a7d68
Riccardo Zanol <ricc@zanol.eu>
2016-05-17 18:49:04 +0200

    Update the transmitter plots

--- matlab/transmitter_plots.m
+++ matlab/transmitter_plots.m
@@ -2,19 +2,21 @@ close all; clear all; clc;
 
 T = 1;
 T_Q = T/4;
-qc_length = 32;
 Qf_samples = 1000;
 
-[b,a] = transmitter_tf();
-q_c = impz(b, a, qc_length);
+[b,a,qc_length,q_c] = transmitter_tf();
 [Q_f_half, f_half] = freqz(b, a, Qf_samples, 1/T_Q);
 
 figure;
 stem((0:length(q_c)-1), q_c);
-xlabel('k');
-ylabel('q_c(k)');
+xlabel('n');
+ylabel('q_c(nT/4)');
+grid on;
+print('plot_qc', '-depsc');
 
 figure;
 plot(f_half, 20*log10(abs(Q_f_half)));
 xlabel('f (Fs = 4/T)');
 ylabel('|Q_c(f)| [dB]');
+grid on;
+print('plot_Qf', '-depsc');
